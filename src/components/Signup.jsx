207465995f3a030a87e1e4c5ebab380f0d00dcfc
aravind <aravindtirumalasetty@gmail.com>
2023-04-06 11:36:52 +0530

    added signup and navbar page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,19 +5,19 @@ function Signuppage() {
     <>
     <div className={`${styles.formcontainer}`}>
     <div className={`${styles.form}`}>
-    <form action="">
-      <div>
-      <label htmlFor="username">User Name: </label>
-      <input type="text" name="username" id="username" />
-      </div>
-      <div>
-      <label htmlFor="email">Email : </label>
-      <input type="email" name="email" id="email" />
-      </div>
-     <div>
-     <label htmlFor="password">Password : </label>
-      <input type="password" name="password" id="password" />
-     </div>
+    <form action="" className={`${styles.formbx}`}>
+      
+      <label htmlFor="username" className={`${styles.label}`}>User Name : </label>
+      <input type="text" name="username" id="username" className={`${styles.inputbx}`} />
+      
+     
+      <label htmlFor="email" className={`${styles.label}`}>Email : </label>
+      <input type="email" name="email" id="email" placeholder='refer@gmail.com' className={`${styles.inputbx}`} />
+      
+     
+     <label htmlFor="password" className={`${styles.label}`}>Password : </label>
+      <input type="password" name="password" id="password" className={`${styles.inputbx}`}/>
+     
      
     </form>
     </div>
