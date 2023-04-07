7b87c21d543381666f49e1648aafbeede7e10087
123bhanu456 <kalahastribahnuprakash@gmail.com>
2023-04-07 21:39:49 +0530

    Feat:Mainpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import {Login} from '../components/Login.jsx';
+import {Login} from './components/Login.jsx';
+import {Main} from './components/Main.jsx';
 
 function App() {
   return (
    <>
-   <Login/>
+   {/* <Login/> */}
+   <Main/>
    </>
   );
 }
